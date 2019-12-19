59dd8cf0ad011ae61ce088ef2af1b20610a63bed
LinLorry <lin864464995@163.com>
2019-12-19 20:30:46 +0800

    :sparkles: Update admin judge.

--- order/js/login.js
+++ order/js/login.js
@@ -1,15 +1,33 @@
 window.localStorage.clear()
 
+function isAdmin() {
+    let url = '/api/user/isAdmin'
+    let headers = {
+        'content-type': 'application/json;charset=UTF-8',
+        'Authorization': 'manage ' + localStorage.getItem('token')
+    }
+
+    fetch(url, {
+            method: 'GET',
+            headers: headers
+        }).then(res => res.json())
+        .then(function (json) {
+            if (json.status === 1) {
+                localStorage.setItem('admin', json.data)
+            }
+        })
+}
+
 function login() {
     let url = '/api/user/token'
     let username = document.getElementById('username');
     let password = document.getElementById('password');
 
     if (username.value.length === 0) {
-        setErrorFor(username,'用户名不能为空！')
+        setErrorFor(username, '用户名不能为空！')
     }
     if (password.value.length === 0) {
-        setErrorFor(password,'密码不能为空！')
+        setErrorFor(password, '密码不能为空！')
         return
     }
 
@@ -19,19 +37,20 @@ function login() {
     };
 
     fetch(url, {
-        body: JSON.stringify(data),
-        headers: {
-            'content-type': 'application/json;charset=UTF-8'
-        },
-        method: 'POST'
-    })
+            body: JSON.stringify(data),
+            headers: {
+                'content-type': 'application/json;charset=UTF-8'
+            },
+            method: 'POST'
+        })
         .then(res => res.json())
-    .then(function(json) {
-        if (json.status === 1) {
-            window.location.href = './home.html'
-            window.localStorage.setItem('token',json.token)
-        } else {
-            console.log(json)
-        }
-    })
+        .then(function (json) {
+            if (json.status === 1) {
+                window.location.href = './home.html'
+                window.localStorage.setItem('token', json.token)
+                isAdmin()
+            } else {
+                console.log(json)
+            }
+        })
 }
