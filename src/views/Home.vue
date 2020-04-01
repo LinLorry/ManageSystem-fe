7b433d3f77d306225bd89456befeab4a94d27b64
LinLorry <lin864464995@163.com>
2020-04-02 00:03:20 +0800

    :children_crossing: Update home view show dynamic info.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,68 @@
         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
       </el-breadcrumb>
     </div>
-    <div></div>
+    <div>
+      <ProductDynamicInfo v-if="hasProduct" :value="productInfo" />
+      <WorkerDynamicInfo
+        v-if="hasWorker"
+        :finish="workerInfo.finishInfo"
+        :finisher="workerInfo.finisherInfo"
+      ></WorkerDynamicInfo>
+    </div>
   </el-card>
 </template>
+
+<script>
+import ProductDynamicInfo from '@/components/ProductDynamicInfo';
+import WorkerDynamicInfo from '@/components/WorkerDynamicInfo';
+
+export default {
+  name: 'Home',
+  components: {
+    ProductDynamicInfo,
+    WorkerDynamicInfo
+  },
+  data() {
+    return {
+      hasProduct: false,
+      hasWorker: false,
+      productInfo: {
+        start: 0,
+        notStart: 0,
+        canComplete: 0,
+        created: 0
+      },
+      workerInfo: {
+        finishInfo: [],
+        finisherInfo: []
+      }
+    };
+  },
+  created() {
+    let _this = this;
+
+    this.axios('/api/user/dynamic').then(res => {
+      if (res.data.data) {
+        const data = res.data.data;
+
+        if (data.product) {
+          _this.hasProduct = true;
+          Object.assign(_this.productInfo, data.product);
+        }
+
+        if (data.worker) {
+          _this.hasWorker = true;
+          Object.assign(_this.workerInfo, data.worker);
+          _this.workerInfo.finishInfo.splice(0, 10);
+          _this.workerInfo.finisherInfo.sort((f, s) => {
+            return s.size - f.size;
+          });
+        }
+      }
+    });
+  },
+  methods: {}
+};
+</script>
 
 <style></style>
