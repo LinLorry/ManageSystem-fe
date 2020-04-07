709a6d216627ba86def657b0cb617d0567398ff2
LinLorry <lin864464995@163.com>
2020-04-07 10:56:00 +0800

    :heavy_plus_sign: Add vant dependency.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import ElementUI from 'element-ui';
+import Vant from 'vant';
 import './axios';
 import 'element-ui/lib/theme-chalk/index.css';
+import 'vant/lib/index.css';
 
 Vue.use(ElementUI);
+Vue.use(Vant);
 
 new Vue({
   router,
