8bdc7d0a2cfa5f64a43c755de7574c3876003dcf
LinLorry <lin864464995@163.com>
2020-04-07 23:01:42 +0800

    :sparkles: Use index tabbar in wechat home views.

--- src/views/WechatIndex.vue
+++ src/views/WechatIndex.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <router-view />
+    <IndexTabbar />
   </div>
 </template>
 
 <script>
+import IndexTabbar from '@/components/IndexTabbar';
+
 export default {
   name: 'WechatIndex',
+  components: {
+    IndexTabbar
+  },
   created() {
     localStorage.setItem('platform', 'wechat');
   }
