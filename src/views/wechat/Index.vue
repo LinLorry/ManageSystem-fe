0cb1879a5671425acc2ea3536f23b106ece08344
LinLorry <lin864464995@163.com>
2020-04-08 19:26:45 +0800

    :sparkles: Use home components and remove product tabbar-item.

--- src/views/wechat/Index.vue
+++ src/views/wechat/Index.vue
@@ -5,24 +5,21 @@
     </keep-alive>
     <van-tabbar route fixed placeholder v-model="active">
       <van-tabbar-item icon="apps-o" name="Home">工作台</van-tabbar-item>
-      <van-tabbar-item v-if="showProduct" icon="orders-o" name="Product"
-        >订单</van-tabbar-item
-      >
       <van-tabbar-item icon="user-o" name="Profile">个人信息</van-tabbar-item>
     </van-tabbar>
   </div>
 </template>
 
 <script>
+import Home from './childComp/Home';
 import Profile from './childComp/Profile';
 
 export default {
   name: 'Index',
-  components: { Profile },
+  components: { Home, Profile },
   data() {
     return {
       active: 'Home',
-      showProduct: false,
       level: []
     };
   },
@@ -32,17 +29,6 @@ export default {
     this.$axios('/api/user/level').then(res => {
       _this.level.splice(0, _this.level.length);
       _this.level.push.apply(_this.level, res.data.data);
-
-      for (const one of _this.level) {
-        if (
-          one === 'admin' ||
-          one === 'product manager' ||
-          one === 'worker manager'
-        ) {
-          _this.showProduct = true;
-          break;
-        }
-      }
     });
   }
 };
