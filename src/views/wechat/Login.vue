221800160545cc5f2f318bfa29434286dc807d5f
LinLorry <lin864464995@163.com>
2020-05-01 21:06:00 +0800

    :bug: Fix when go back with code can't goto home view bug.

--- src/views/wechat/Login.vue
+++ src/views/wechat/Login.vue
@@ -10,20 +10,24 @@ export default {
       const _this = this;
       const preUrl = this.$route.query.preUrl;
 
-      this.axios('/api/wechat/login?code=' + code).then(res => {
-        const token = res.data.token;
-        localStorage.setItem('wechatUserInfo', JSON.stringify(res.data.data));
-        if (token) {
-          localStorage.setItem('token', token);
-          if (preUrl) {
-            _this.$router.push({ path: preUrl });
+      this.axios('/api/wechat/login?code=' + code)
+        .then(res => {
+          const token = res.data.token;
+          localStorage.setItem('wechatUserInfo', JSON.stringify(res.data.data));
+          if (token) {
+            localStorage.setItem('token', token);
+            if (preUrl) {
+              _this.$router.push({ path: preUrl });
+            } else {
+              _this.$router.push('/wechat');
+            }
           } else {
-            _this.$router.push('/wechat');
+            _this.$router.push('/wechat/info');
           }
-        } else {
-          _this.$router.push('/wechat/info');
-        }
-      });
+        })
+        .catch(() => {
+          _this.$router.push('/wechat');
+        });
     } else {
       location.href =
         'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx8eb0addaf4641342&redirect_uri=' +
